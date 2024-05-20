d94bad580ab7aed027869f18f4d9edbbda1c66c2
Santosh Kumar Raut <147261516+santosh-rawat-prof@users.noreply.github.com>
2024-05-20 12:46:34 +0530

    Time: 104 ms (85.43%), Space: 53.9 MB (92.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,59 +2,92 @@
  * @param {string} s
  * @return {number}
  */
+
+// Brute Force 
+// var romanToInt = function(s) {
+//     const romans = s.split("");
+//     let prev = 0;
+//     let convertedNumber = 0;
+//     romans.map((roman) => {
+//         if(roman === "I") {
+//             convertedNumber += 1;
+//             prev = 1;
+//         } else if(roman === "V") {
+//             if(prev === 1) {
+//                 convertedNumber += 3;
+//             } else {
+//                 convertedNumber += 5;
+//             }
+//         }
+//         else if(roman === "X") {
+//             if(prev === 1) {
+//                 convertedNumber += 8
+//             } else {
+//                 convertedNumber += 10;
+//                 prev = 10;
+//             }
+//         }
+//         else if(roman === "L") {
+//             if(prev === 10) {
+//                 convertedNumber += 30;
+//             } else {
+//                 convertedNumber += 50;
+//             }
+//         }
+//         else if(roman === "C") {
+//             if(prev === 10) {
+//                 convertedNumber += 80;
+//             } else {
+//                 convertedNumber += 100;
+//                 prev = 100;
+//             }
+//         }
+//         else if(roman === "D") {
+//             if(prev === 100) {
+//                 convertedNumber += 300;
+//             } else {
+//                 convertedNumber += 500;
+//             }
+//         }
+//         else if(roman === "M") {
+//             if(prev === 100) {
+//                 convertedNumber += 800;
+//             } else {
+//                 convertedNumber += 1000;
+//             }
+//         }
+//     })
+//     console.log(romans);
+//     return convertedNumber;
+// };
+
+// Optimized
+
 var romanToInt = function(s) {
-    const romans = s.split("");
-    let prev = 0;
+    const romanToIntMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
     let convertedNumber = 0;
-    romans.map((roman) => {
-        if(roman === "I") {
-            convertedNumber += 1;
-            prev = 1;
-        } else if(roman === "V") {
-            if(prev === 1) {
-                convertedNumber += 3;
-            } else {
-                convertedNumber += 5;
-            }
+    let prevValue = 0;
+    
+    for (let i = s.length - 1; i >= 0; i--) {
+        let currentValue = romanToIntMap[s[i]];
+        
+        if (currentValue >= prevValue) {
+            convertedNumber += currentValue;
+        } else {
+            convertedNumber -= currentValue;
         }
-        else if(roman === "X") {
-            if(prev === 1) {
-                convertedNumber += 8
-            } else {
-                convertedNumber += 10;
-                prev = 10;
-            }
-        }
-        else if(roman === "L") {
-            if(prev === 10) {
-                convertedNumber += 30;
-            } else {
-                convertedNumber += 50;
-            }
-        }
-        else if(roman === "C") {
-            if(prev === 10) {
-                convertedNumber += 80;
-            } else {
-                convertedNumber += 100;
-                prev = 100;
-            }
-        }
-        else if(roman === "D") {
-            if(prev === 100) {
-                convertedNumber += 300;
-            } else {
-                convertedNumber += 500;
-            }
-        }
-        else if(roman === "M") {
-            if(prev === 100) {
-                convertedNumber += 800;
-            } else {
-                convertedNumber += 1000;
-            }
-        }
-    })
-    console.log(romans);
+        
+        prevValue = currentValue;
+    }
+    
     return convertedNumber;
 };
